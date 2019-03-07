59ec4fadcd9ed1154b70cad0950ad868d328bc9d
ravikirany711 <ravikirany711@gmail.com>
2019-03-07 12:30:45 +0530

    passport jwt authentication strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport=require('passport')
 const router = express.Router();
 
 const User = require("../../models/User");
@@ -68,20 +69,35 @@ router.post("/login", (req, res) => {
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; //create jwt PAYLOAD
 
         //Sign token
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 });
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 },(err,token)=>{
+          res.json({
+            success:true,
+            token:"Bearer " +token
+          })
+
+        });
       } else {
         return res
           .status(400)
-          .json({ password: "Password incorrect" }, (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer" + token
-            });
-          });
+          .json({ password: "Password incorrect" })
+            
       }
     });
   });
 });
+
+
+//@router GET api/users/current
+//@desc   Return the current user
+//@access Private
+router.get('/current',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  res.json({
+    id:req.user.id,
+    name:req.user.name,
+    email:req.user.email
+  })
+  
+})
 
 //we need to export it in order to use in server.js
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport=require('passport')
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -21,9 +22,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("express home route set up and working as expected");
-});
+//Passport middleware
+app.use(passport.initialize())
+//Passport config
+require('./config/passport')(passport)
 
 //Use routes
 app.use("/api/users", users);
